927f32a7b1c6a4a89832ee229853af27f985e9da
Sidra Waseem <sidraw24@gmail.com>
2022-09-23 09:14:36 +0500

    update api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Question from './components/Questions/Questions.jsx';
 
-// https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple
+
 
 function App() {
+  const [question, setQuestion] = React.useState('');
+
+// https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple
+fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
+.then((response) => response.json())
+.then((data) => setQuestion(data.results.question));
+console.log(question);
   return (
     <div>
      <Question/>
